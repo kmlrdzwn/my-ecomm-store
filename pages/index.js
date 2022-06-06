3266767e0bf8caccf8ddbb43b7d2889cf30f7378
kmlrdzwn <kamal.redzwan@gmail.com>
2022-06-06 14:34:41 +0800

    Update Desc

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
       <main className={styles.main}>
         <h1 className={styles.title}>Space Jelly Shop</h1>
 
-        <p className={styles.description}>The best space jellyfish swag on the web!</p>
+        <p className={styles.description}>The best space jellyfish swag in the universe!</p>
 
         <ul className={styles.grid}>
           {products.map((product) => {
